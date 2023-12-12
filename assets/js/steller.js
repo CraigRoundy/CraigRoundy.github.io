f9b4162bd1e43d4a16cc76389312ad62cb27a4fd
CraigRoundy <croundy11@gmail.com>
2023-12-12 12:56:10 -0700

    js mods

--- assets/js/steller.js
+++ assets/js/steller.js
@@ -1,32 +1,32 @@
-/*!
-=========================================================
-* Steller Landing page
-=========================================================
 
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-// smooth scroll
+// scroll smoothly
 $(document).ready(function(){
-	$(".nav-link").on('click', function(event) {
-
-    	if (this.hash !== "") {
-
+	$("a").on('click', function(event) {
+		if (this.hash !== "") {
 			event.preventDefault();
-
 			var hash = this.hash;
-
-			$('html, body').animate({
-				scrollTop: $(hash).offset().top
-			}, 700, function(){
+			$('html, body').animate({scrollTop: $(hash).offset().top}, 800, function(){
 				window.location.hash = hash;
 			});
-      	} 
-    });
+		}
+	});
 });
+
+// scrollspy
+$('body').scrollspy({ target: '#navbar' });
+
+// scroll to top
+$(document).ready(function(){
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scroll-to-top').fadeIn();
+		} else {
+			$('.scroll-to-top').fadeOut();
+		}
+	});
+	$('.scroll-to-top').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+});
+
