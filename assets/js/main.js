142eb4750b721cbe37b3c300cd21ccfa0a143c05
CraigRoundy <croundy11@gmail.com>
2023-12-15 15:23:57 -0700

    Stuff

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+// main.js
+
+// For now we can use this file to import all the JS files individually
+// Break them down later if needed
+
+// Import other JS files
+import './assets/js/layout/header.js';
+import './assets/js/layout/footer.js';
+import './assets/js/layout/scroll.js';
+import './assets/js/layout/scrolltop.js';
+import './assets/js/layout/section.js';
+
+import './assets/js/components/accordion.js';
+import './assets/js/components/carousel.js';
+import './assets/js/components/datepicker.js';
+import './assets/js/components/dropdown.js';
+import './assets/js/components/modal.js';
+import './assets/js/components/scrollspy.js';
+import './assets/js/components/tab.js';
+import './assets/js/components/tooltip.js';
+import './assets/js/components/typed.js';
+
+import './assets/js/pages/home.js';
+import './assets/js/pages/about.js';
+import './assets/js/pages/contact.js';
+import './assets/js/pages/faq.js';
+import './assets/js/pages/login.js';
+import './assets/js/pages/register.js';
+import './assets/js/pages/reset.js';
+import './assets/js/pages/404.js';
+import './assets/js/pages/500.js';
+import './assets/js/pages/blank.js';
+import './assets/js/pages/comingsoon.js';
+import './assets/js/pages/confirmation.js';
+import './assets/js/pages/lock.js';
+import './assets/js/pages/pricing.js';
+import './assets/js/pages/profile.js';
+import './assets/js/pages/search.js';
+import './assets/js/pages/settings.js';
+
+import './assets/js/base/topography.js';
+
+import './assets/js/vendors/jquery/jquery-3.4.1.js';
+import './assets/js/vendors/bootstrap/bootstrap.bundle.js';
+import './assets/js/vendors/bootstrap/bootstrap.affix.js';
+import './assets/js/cr.js';
+
+// Main code?
